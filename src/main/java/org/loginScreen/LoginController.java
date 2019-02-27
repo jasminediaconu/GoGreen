3cb978f3dcef81f1c9eb29111cef12f4b10ac7ad
Giuliano Forghieri <g.forghieri@student.tudelft.nl>
2019-02-28 00:53:27 +0100

    login screen, signup screen, signup functionality SQLite db, main dummy screen

--- src/main/java/org/loginScreen/LoginController.java
+++ src/main/java/org/loginScreen/LoginController.java
@@ -1,11 +1,113 @@
 package org.loginScreen;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
+    @FXML
+    void close(MouseEvent event) {
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.close();
+    }
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    private PasswordField pf_password;
+
+    double x = 0, y = 0;
+
+    @FXML
+    void pressed(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
+
+    @FXML
+    void dragged(MouseEvent event) {
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+    @FXML
+    void login(MouseEvent event) throws SQLException, IOException {
+
+        String username, password;
+
+        username = tf_username.getText();
+        password = pf_password.getText();
+
+        Connection connection = DBConnect.getInstance().getConnection();
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery( "select * from users where username" +
+                " = '"+username+"' or email = '" + pf_password + "' and password = '" + password +"'");
+
+        if (resultSet.next()) {
+            Parent root = FXMLLoader.load(getClass().getResource("mainScreenDummy.fxml"));
+
+            Node node = (Node) event.getSource();
+
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+
+            scene.setFill(Color.TRANSPARENT);
+            stage.initStyle(StageStyle.TRANSPARENT);
+
+        }
+    }
+
+    @FXML
+    void signup(MouseEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("signup.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+
+        scene.setFill(Color.TRANSPARENT);
+
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
---
+++ src/main/java/org/loginScreen/SignupController.java
@@ -0,0 +1,125 @@
+package org.loginScreen;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class SignupController implements Initializable {
+
+    @FXML
+    private FontAwesomeIcon close;
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    private TextField tf_email;
+
+    @FXML
+    private PasswordField pf_password;
+
+    double x = 0, y = 0;
+
+    @FXML
+    void close(MouseEvent event) {
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.close();
+    }
+
+    @FXML
+    void pressed(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
+
+    @FXML
+    void dragged(MouseEvent event) {
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+    @FXML
+    void login(MouseEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+
+        scene.setFill(Color.TRANSPARENT);
+    }
+
+    @FXML
+    void signup(MouseEvent event) {
+
+        Connection connection = DBConnect.getInstance().getConnection();
+
+        try {
+
+            String username = tf_username.getText();
+            String email = tf_email.getText();
+            String password = pf_password.getText();
+
+
+            Statement statement = connection.createStatement();
+
+            int status = statement.executeUpdate("insert into users (username, email, password)" +
+                    "values ('"+username+"','"+email+"','"+password+"')");
+
+            if (status > 0) {
+                System.out.println("user registered ");
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+
+
+
+
+
+    }
+}

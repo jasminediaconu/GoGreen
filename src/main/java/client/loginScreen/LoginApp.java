d9a4be59d92bd73573681eb8efe93e3a483ebd11
Giuliano Forghieri <g.forghieri@student.tudelft.nl>
2019-03-02 22:55:02 +0100

    gitignore update to fix pipeline CI error, renamed LoginScreen class because having the same name as the packagename. Renamed to LoginApp

---
+++ src/main/java/client/loginScreen/LoginApp.java
@@ -0,0 +1,41 @@
+package client.loginscreen;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class LoginScreen extends Application {
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Scene scene = new Scene(root);
+
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}

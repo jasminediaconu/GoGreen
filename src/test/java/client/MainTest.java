7054166f1886ec35c0cb1490e008caf79a332ab9
Wout Haakman <w.g.haakman@student.tudelft.nl>
2019-03-05 14:01:19 +0100

    Last test CI failed successfully, now it should work again

--- src/test/java/client/MainTest.java
+++ src/test/java/client/MainTest.java
@@ -15,7 +15,7 @@ public class MainTest {
 
     @Test
     public void testHashPasswordRaw(){
-        Assert.assertEquals("e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", Main.hashString("password"));
+        Assert.assertEquals("5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", Main.hashString("password"));
     }
 
     @Test

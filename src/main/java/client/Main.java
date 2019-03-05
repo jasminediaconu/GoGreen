58f394614343cd95275a7063c5862689f4a9e2a8
Giuliano Forghieri <g.forghieri@student.tudelft.nl>
2019-03-05 15:16:48 +0100

    Renamed LoginsCreen as LoginApp class, pipeline now should suceed.

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public static String sessionID;
 
     public static void main(String[] args) {
-        Application.launch(LoginScreen.class, args);
+        Application.launch(LoginApp.class, args);
         ServerRequests.endSession();
     }
 

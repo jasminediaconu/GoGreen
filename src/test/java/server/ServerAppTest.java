79709adedd4adc96a70958b20624a6b567397eb0
Wout Haakman <w.g.haakman@student.tudelft.nl>
2019-03-09 16:53:24 +0100

    Removed unnecessary classes

--- src/test/java/server/ServerAppTest.java
+++ src/test/java/server/ServerAppTest.java
@@ -1,28 +1,42 @@
 package server;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
 
 class ServerAppTest {
 
     @Test
-    void main() {
-    }
-
-    @Test
     void createNewSessionID() {
+        Assert.assertEquals(UUID.randomUUID().toString(), ServerApp.createNewSessionID());
+        Assert.assertEquals(UUID.randomUUID().toString(), ServerApp.createNewSessionID());
     }
 
     @Test
     void addSessionID() {
+        String uuid = ServerApp.createNewSessionID();
+        ServerApp.addSessionID(uuid, 2);
+        Assert.assertEquals(ServerApp.getUserIDFromSession(uuid), 2);
     }
 
     @Test
     void removeSessionID() {
+        String uuid = ServerApp.createNewSessionID();
+        ServerApp.addSessionID(uuid, 5);
+        Assert.assertEquals(ServerApp.getUserIDFromSession(uuid), 5);
+        ServerApp.removeSessionID(uuid);
+        Assert.assertEquals(ServerApp.getUserIDFromSession(uuid), null);
     }
 
     @Test
     void getUserIDFromSession() {
+        String uuid1 = ServerApp.createNewSessionID();
+        ServerApp.addSessionID(uuid1, 12);
+        Assert.assertEquals(ServerApp.getUserIDFromSession(uuid1), 12);
+        String uuid2 = ServerApp.createNewSessionID();
+        ServerApp.addSessionID(uuid2, 14);
+        Assert.assertEquals(ServerApp.getUserIDFromSession(uuid1), 12);
+        Assert.assertEquals(ServerApp.getUserIDFromSession(uuid2), 14);
     }
 }

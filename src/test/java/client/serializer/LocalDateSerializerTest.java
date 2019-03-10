9b5e6cf57226e4b2929c4e13f3b7e30316b39bb9
Wout Haakman <w.g.haakman@student.tudelft.nl>
2019-03-10 20:37:34 +0100

    Serializer and Deserializer tests

--- src/test/java/client/serializer/LocalDateSerializerTest.java
+++ src/test/java/client/serializer/LocalDateSerializerTest.java
@@ -1,10 +1,18 @@
 package client.serializer;
 
+import com.google.gson.Gson;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 class LocalDateSerializerTest {
 
     @Test
     void serialize() {
+        LocalDate localDate = LocalDate.now().plusDays(25);
+        String json = new Gson().toJson(localDate);
+        LocalDate deserialized = new Gson().fromJson(json, LocalDate.class);
+        Assert.assertEquals(localDate, deserialized);
     }
 }

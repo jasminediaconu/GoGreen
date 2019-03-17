4ce082fd07c8ffbbb09d1e0f222ce01d2d6eca2d
Mandy Chang <m.w.chang@student.tudelft.nl>
2019-03-17 18:34:52 +0100

    changed dummy users with global best users

--- src/main/java/client/windows/LeaderboardController.java
+++ src/main/java/client/windows/LeaderboardController.java
@@ -1,6 +1,39 @@
 package client.windows;
 
-public class LeaderboardController extends Controller {
+import client.ServerRequests;
+import javafx.fxml.Initializable;
+import client.user.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LeaderboardController extends Controller implements Initializable {
+
+    @FXML public TableView<User> table;
+    @FXML public TableColumn<User, String> usernameColumn;
+    @FXML public TableColumn<User, String> countryColumn;
+    @FXML public TableColumn<User, Double> totalCo2Column;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("username"));
+        countryColumn.setCellValueFactory(new PropertyValueFactory<User, String>("country"));
+        totalCo2Column.setCellValueFactory(new PropertyValueFactory<User, Double>("totalCo2"));
+
+        table.setItems(getUsers());
+    }
+
+    public ObservableList<User> getUsers(){
+        ObservableList<User> oListUsers = FXCollections.observableArrayList(ServerRequests.getGlobalBestProfile());
+
+        return oListUsers;
+    }
+
+
     @Override
     public void update() {
 
--- src/main/java/client/windows/MainScreen.java
+++ src/main/java/client/windows/MainScreen.java
@@ -4,22 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
-/**
- * The type Main screen.
- */
 public class MainScreen extends Application {
 
     private static Stage primaryStage;
 
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
@@ -35,20 +28,18 @@ public class MainScreen extends Application {
 
     /**
      * This function will start the Application wrapper for the MainScreen, sets the size and title.
-     *
      * @param primaryStage The primary stage
-     * @throws Exception Exception.
+     * @throws Exception
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        MainScreen.primaryStage = primaryStage;
-        //Parent root = FXMLLoader.load(getClass().getResource("/client/windows/fxml/mainScreen.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("fxml/mainScreen.fxml"));
         primaryStage.setTitle("GoGreen");
         primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.getScene().setFill(Color.TRANSPARENT);
         primaryStage.setResizable(false);
         primaryStage.show();
-        primaryStage.getIcons().add(new Image("/client/windows/images/icon.png"));
-    }
-}
 
+    }
+
+}

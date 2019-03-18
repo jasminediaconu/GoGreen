21f591c20fda9fcc52920d68c5d612831d1b9dee
Mandy Chang <m.w.chang@student.tudelft.nl>
2019-03-18 12:12:11 +0100

    add follow buttons

--- src/main/java/client/windows/LeaderboardController.java
+++ src/main/java/client/windows/LeaderboardController.java
@@ -1,14 +1,19 @@
 package client.windows;
 
 import client.ServerRequests;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import client.user.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -24,15 +29,56 @@ public class LeaderboardController extends Controller implements Initializable {
         countryColumn.setCellValueFactory(new PropertyValueFactory<User, String>("country"));
         totalCo2Column.setCellValueFactory(new PropertyValueFactory<User, Double>("totalCo2"));
 
+        addFollowButtons();
         table.setItems(getUsers());
     }
 
     public ObservableList<User> getUsers(){
-        ObservableList<User> users = FXCollections.observableArrayList(ServerRequests.getGlobalBestProfile());
-
-
+        ServerRequests serverRequests = new ServerRequests();
+        ObservableList<User> users = FXCollections.observableArrayList(serverRequests.getGlobalBestProfile());
 
         return users;
+    }
+
+    public void addFollowButtons(){
+        TableColumn<User, Void> followButtonColumn = new TableColumn<>("Follow");
+
+        Callback<TableColumn<User, Void>, TableCell<User, Void>> cellFactory = new Callback<TableColumn<User, Void>, TableCell<User, Void>>() {
+            @Override
+            public TableCell<User, Void> call(final TableColumn<User, Void> param) {
+                final TableCell<User, Void> cell = new TableCell<User, Void>() {
+
+                    private final Button followButton = new Button("follow");
+
+                    {
+                        followButton.setOnAction((ActionEvent event) -> {
+                            User data = getTableView().getItems().get(getIndex());
+                            System.out.println("selectedData: " + data);
+                            if (followButton.getText() == "unfollow"){
+                                followButton.setText("follow");
+                            } else {
+                                followButton.setText("unfollow");
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(followButton);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        followButtonColumn.setCellFactory(cellFactory);
+
+        table.getColumns().add(followButtonColumn);
     }
 
 

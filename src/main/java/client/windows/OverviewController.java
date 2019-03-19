6d9780517ef76e57490a8dca2b1e733fa6cb3854
Jasmine Diaconu <j.c.diaconu@student.tudelft.nl>
2019-03-19 15:56:26 +0100

    Fixed OverviewController

--- src/main/java/client/windows/OverviewController.java
+++ src/main/java/client/windows/OverviewController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Tooltip;
 
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -19,6 +18,8 @@ import java.util.ResourceBundle;
 
 public class OverviewController extends Controller implements Initializable {
 
+    List<JFXButton> badges;
+
     @FXML
     Pane overview;
 
@@ -28,20 +29,12 @@ public class OverviewController extends Controller implements Initializable {
     private VBox badgesBox;
     private HBox badgesColumn;
 
-    List<JFXButton> badges;
-
     @Override
     public void update() {
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        final Tooltip tooltip = new Tooltip();
-        final Tooltip tooltipDisable = new Tooltip();
-
-        tooltip.setText("You logged in 5 days in a row!");
-        tooltipDisable.setText("You don't have this badge yet.");
 
         badgesBox = new VBox();
         badgesBox.setPadding(new Insets(20, 0, 0, 20));
@@ -50,38 +43,13 @@ public class OverviewController extends Controller implements Initializable {
 
         badges = new ArrayList<>();
 
-        String path= "/client/windows/images/badges/";
-        JFXButton button = new JFXButton("", new ImageView(path + "hands.png"));
-        JFXButton button2 = new JFXButton("", new ImageView(path + "energy.png"));
-        JFXButton button3 = new JFXButton("", new ImageView(path + "medal.png"));
-        JFXButton button4 = new JFXButton("", new ImageView(path + "sprout.png"));
-        JFXButton button5 = new JFXButton("", new ImageView(path + "drop.png"));
-        JFXButton button6 = new JFXButton("", new ImageView(path + "earth.png"));
-        JFXButton button7 = new JFXButton("", new ImageView(path + "green-energy.png"));
-        JFXButton button8 = new JFXButton("", new ImageView(path + "leds.png"));
-        JFXButton button9 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button10 = new JFXButton("", new ImageView(path + "radiator.png"));
-        JFXButton button11 = new JFXButton("", new ImageView(path + "recycle.png"));
-
-        button2.setTooltip(tooltip);
-        button3.setTooltip(tooltipDisable);
-        button4.setTooltip(tooltip);
-
-        button3.setDisable(true);
+        String path = "/client/windows/images/badges/";
+        JFXButton button = new JFXButton("", new ImageView(path + "solar-panel.png"));
 
         badges.add(button);
-        badges.add(button2);
-        badges.add(button3);
-        badges.add(button4);
-        badges.add(button5);
-        badges.add(button6);
-        badges.add(button7);
-        badges.add(button8);
-        badges.add(button9);
-        badges.add(button10);
-        badges.add(button11);
 
-        for(int i=0; i<badges.size(); i++ ) {
+
+        for (int i = 0; i < badges.size(); i++ ) {
             badgesColumn.getChildren().add(i, badges.get(i));
         }
 

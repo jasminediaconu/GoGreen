4b79ff95a008de3afed1515cb4228ae168ff0e0f
Jasmine Diaconu <j.c.diaconu@student.tudelft.nl>
2019-03-20 13:11:35 +0100

    Badges showing up on the Overview, change opacity when badge is achieved

--- src/main/java/client/windows/OverviewController.java
+++ src/main/java/client/windows/OverviewController.java
@@ -48,20 +48,20 @@ public class OverviewController extends Controller implements Initializable {
 
         String path = "/client/windows/images/badges/";
         JFXButton button = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button2 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button3 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button4 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button5 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button6 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button7 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button8 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button9 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button10 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button11 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button12 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button13 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button14 = new JFXButton("", new ImageView(path + "solar-panel.png"));
-        JFXButton button15 = new JFXButton("", new ImageView(path + "solar-panel.png"));
+        JFXButton button2 = new JFXButton("", new ImageView(path + "solar-panels.png"));
+        JFXButton button3 = new JFXButton("", new ImageView(path + "burger.png"));
+        JFXButton button4 = new JFXButton("", new ImageView(path + "vegetarian.png"));
+        JFXButton button5 = new JFXButton("", new ImageView(path + "stalker.png"));
+        JFXButton button6 = new JFXButton("", new ImageView(path + "top10.png"));
+        JFXButton button7 = new JFXButton("", new ImageView(path + "thermometer.png"));
+        JFXButton button8 = new JFXButton("", new ImageView(path + "travel.png"));
+        JFXButton button9 = new JFXButton("", new ImageView(path + "activity.png"));
+        JFXButton button10 = new JFXButton("", new ImageView(path + "one.png"));
+        JFXButton button11 = new JFXButton("", new ImageView(path + "progress.png"));
+        JFXButton button12 = new JFXButton("", new ImageView(path + "bronze-medal.png"));
+        JFXButton button13 = new JFXButton("", new ImageView(path + "silver-medal.png"));
+        JFXButton button14 = new JFXButton("", new ImageView(path + "gold-medal.png"));
+        JFXButton button15 = new JFXButton("", new ImageView(path + "nomad.png"));
 
         badges.add(button);
         badges.add(button2);
@@ -79,8 +79,10 @@ public class OverviewController extends Controller implements Initializable {
         badges.add(button14);
         badges.add(button15);
 
+        button.setStyle("-fx-opacity: 100%;");
+
+        // This adds the badges to the different rows of the VBOX
         for(int i = 0; i < 5; i++){
-            //ADD 3 HBOXES
             row.getChildren().add(i, badges.get(i));
             row2.getChildren().add(i, badges.get(i+5));
             row3.getChildren().add(i, badges.get(i+10));

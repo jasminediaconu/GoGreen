19c269e60a00fa90a8d5dd79fdbd4f78592ee996
Jan Willem Eriks <j.w.a.eriks@student.tudelft.nl>
2019-03-25 10:31:08 +0100

    Activity deletion and welcome message fix

--- src/main/java/client/windows/MainScreenController.java
+++ src/main/java/client/windows/MainScreenController.java
@@ -1,5 +1,6 @@
 package client.windows;
 
+import client.Main;
 import javafx.animation.FadeTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
@@ -46,8 +47,15 @@ public class MainScreenController extends Pane implements Initializable {
     private ArrayList<Controller> controllers = new ArrayList<>();
     @FXML
     private AnchorPane mainPane;
+
     @FXML
     private Pane welcomePane;
+    @FXML
+    private Text dayField;
+    @FXML
+    private Text userNameField;
+
+
     @FXML
     private MenuItem logoutButton;
 
@@ -279,6 +287,8 @@ public class MainScreenController extends Pane implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        userNameField.setText(Main.clientUser.getUsername());
+        dayField.setText("" + Main.clientUser.getStreakLength());
         for (Controller controller : controllers) {
             controller.init();
         }

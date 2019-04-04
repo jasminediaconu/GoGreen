9266b5814b1145d3a53e4103d41a743990af1ddd
Jan Willem Eriks <j.w.a.eriks@student.tudelft.nl>
2019-04-04 15:40:45 +0200

    kg added to CO2 saved in Profile

--- src/main/java/client/profilescreen/ProfileController.java
+++ src/main/java/client/profilescreen/ProfileController.java
@@ -88,7 +88,7 @@ public class ProfileController extends Controller {
     private void syncUI(ClientUser settings) {
         usernameField.setText("Username: " + settings.getUsername());
         emailField.setText(settings.getEmail());
-        pointsField.setText("CO2 saved: " + Main.round(Main.clientUser.getTotalCo2(), 2));
+        pointsField.setText("CO2 saved: " + Main.round(Main.clientUser.getTotalCo2(), 2) + " kg");
         streakField.setText("Streak: " + Main.clientUser.getStreakLength());
         solarPanelsField.setText("" + Main.clientUser.getSolarPower());
         ledsField.setText("" + Main.clientUser.getLeds());
